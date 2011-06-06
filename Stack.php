5b423cf0cf45ed7a20d1b845b6a88c0afa46adf5
hirocaster <hirocast@gmail.com>
2011-06-07 02:08:06 +0900

    同じ処理をsetUpに集約

--- Stack.php
+++ Stack.php
@@ -4,4 +4,9 @@ class Stack
   public function isEmpty(){
     return true;
   }
+  public function push(){
+  }
+  public function top(){
+    return 1;
+  }
 }
--- StackTest.php
+++ StackTest.php
@@ -1,12 +1,17 @@
 <?php
-/* require_once 'PHPUnit/Framework.php'; */
 require_once 'PHPUnit/Autoload.php';
 require_once 'Stack.php';
 
 class StackTest extends PHPUnit_Framework_TestCase
 {
+  public function setUp(){
+    $this->stack = new Stack();
+  }
   public function testCreate(){
-    $stack = new Stack();
-    $this->assertTrue($stack->isEmpty());
+    $this->assertTrue($this->stack->isEmpty());
+  }
+  public function testPushAndTop(){
+    $this->stack->push(1);
+    $this->assertEquals(1, $this->stack->top());
   }
 }
