e26e0db7ed9445cae1b6386482e266d020b93b1c
hirocaster <hirocast@gmail.com>
2011-06-07 01:59:42 +0900

    NOTICEが出ていたのを修正

--- StackTest.php
+++ StackTest.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'PHPUnit/Framework.php';
+/* require_once 'PHPUnit/Framework.php'; */
+require_once 'PHPUnit/Autoload.php';
 require_once 'Stack.php';
 
 class StackTest extends PHPUnit_Framework_TestCase
