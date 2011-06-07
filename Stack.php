4bdf0cceb0d9f861b31a37601448e556bc028381
hirocaster <hirocast@gmail.com>
2011-06-07 23:31:56 +0900

    4終了

--- Stack.php
+++ Stack.php
@@ -2,16 +2,22 @@
 class Stack
 {
   public function isEmpty(){
-    return true;
+    return $this->size === 0;
   }
 
   private $value = 0;
+  private $size = 0;
 
   public function push($value){
     $this->value = $value;
+    ++$this->size;
   }
 
   public function top(){
     return $this->value;
+  }
+
+  public function size(){
+    return $this->size;
   }
 }
--- StackTest.php
+++ StackTest.php
@@ -7,11 +7,21 @@ class StackTest extends PHPUnit_Framework_TestCase
   public function setUp(){
     $this->stack = new Stack();
   }
+
   public function testCreate(){
     $this->assertTrue($this->stack->isEmpty());
   }
+
   public function testPushAndTop(){
     $this->stack->push(1);
+    $this->assertFalse($this->stack->isEmpty());
     $this->assertEquals(1, $this->stack->top());
+  }
+
+  public function testPushAndSize(){
+    $this->stack->push(1);
+    $this->assertEquals(1, $this->stack->size());
+    $this->stack->push(2);
+    $this->assertEquals(2, $this->stack->size());
   }
 }
